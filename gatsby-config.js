f82f4a5fe52a643e507e66bf6d5fc666e74ca3ec
viktmv <viktmv@icloud.com>
2020-05-18 11:15:25 -0400

    Switch fav image to png

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/favicon.ico`
+        icon: `content/assets/flame.png`
       },
     },
     `gatsby-plugin-offline`,
